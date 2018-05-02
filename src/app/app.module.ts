67d51d5c65b6ec3b363ba5ffadd0e73cf54d2ce1
sergiofra98 <sergiofra98@hotmail.com>
2018-05-01 23:26:31 -0500

    primera version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { FrutaComponent } from './fruta/fruta.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FrutaComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/fruta/fruta.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fruta',
+  templateUrl: './fruta.component.html',
+  styleUrls: ['./fruta.component.css']
+})
+export class FrutaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
